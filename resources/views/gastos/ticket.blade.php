af10d57d4f5767a169582918e295002e0e6aafab
Erik Zerpa <ezerpa@sipecom.com>
2022-07-21 13:22:27 -0400

    aaaa

--- resources/views/gastos/ticket.blade.php
+++ resources/views/gastos/ticket.blade.php
@@ -35,55 +35,17 @@
     <body style="width:100%; position:fixed: top: 1px; ">
 
     <br><br>
-
-	<div class="" style="font-size: 35px; text-align: center;margin-bottom: 15px;">
-		<img src="/var/www/html/sistemaIntimas/public/image.png"  style="width: 40%; color: black;"/>
+    <div class="" style="font-size: 35px; text-align: center;margin-bottom: 15px;">
+		<img src="/var/www/html/intimas/public/logo_negro.jpg"  style="width: 40%; color: black;"/>
 	</div>
 
     <div  style="font-size: 15px; text-align: center;margin-bottom:-60px;margin-top: -30px;">
-    @if(Session::get('sedeName') == 'PROCERES')
-
-		<p><strong>MADRE TERESA SAC-{{Session::get('sedeName')}}</strong></p>
-		<p style="margin-top: -20px;"><strong>RUC: 20600971116</strong></p>
-        <p style="margin-top: -10px;"><strong>Dirección: Av Próceres de la independencia 1781
-3er piso SJL</strong></p>
-		<p style="margin-top: -20px;"><strong>Teléfono:  01 3764637</strong></p>
-		<p style="margin-top: -20px;"><strong>WhatsApp: 978 216 135</strong></p>
-   @elseif(Session::get('sedeName') == 'CANTO REY')
-   <p><strong>MADRE TERESA SAC-{{Session::get('sedeName')}}</strong></p>
-		<p style="margin-top: -20px;"><strong>RUC: 20600971116</strong></p>
-        <p style="margin-top: -10px;"><strong>Dirección: Av Wiesse 3331 2do piso  SJL</strong></p>
-		<p style="margin-top: -20px;"><strong>Teléfono:  01 2534502</strong></p>
-		<p style="margin-top: -20px;"><strong>WhatsApp: 978 216 135</strong></p>
-        @elseif(Session::get('sedeName') == 'VIDA FELIZ')
-   <p><strong>VIDA FELIZ SAC- MADRE TERESA</strong></p>
-		<p style="margin-top: -20px;"><strong>RUC: 20602415539</strong></p>
-        <p style="margin-top: -10px;"><strong>Dirección: Av Próceres de la independencia 1795
-2do piso SJL</strong></p>
-		<p style="margin-top: -20px;"><strong>Teléfono:  01 4596494</strong></p>
-		<p style="margin-top: -20px;"><strong>WhatsApp: 978 216 315</strong></p>
-    @elseif(Session::get('sedeName') == 'INDEPENDENCIA')
-   <p><strong>SYSMEDIC PERU SAC -INDEPENDENCIA</strong></p>
-		<p style="margin-top: -20px;"><strong>RUC: 20606283980</strong></p>
-        <p style="margin-top: -10px;"><strong>Dirección: AV.CHINCHAYSUYO 323 TAHUANTINSUYO</strong></p>
-		<p style="margin-top: -20px;"><strong>Teléfono: 01 5265711</strong></p>
-		<p style="margin-top: -20px;"><strong>WhatsApp: 940 309 507</strong></p>
-    @elseif(Session::get('sedeName') == 'ZARATE')
-    <p><strong>SYSMEDIC PERU SAC</strong></p>
-    <p style="margin-top: -20px;"><strong>RUC: 20606283980</strong></p>
-        <p style="margin-top: -10px;"><strong>Dirección:  Av. gran chimu 745 Zarate SJL</strong></p>
-		<p style="margin-top: -20px;"><strong>Teléfono:   01 7820512</strong></p>
-		<p style="margin-top: -20px;"><strong>WhatsApp: 924 520 026</strong></p>
-
-    @else
-   <p><strong>SYSMEDIC PERU SAC -LOS OLIVOS</strong></p>
-		<p style="margin-top: -20px;"><strong>RUC: 20606283980</strong></p>
-        <p style="margin-top: -10px;"><strong>Dirección: AV.PROCERES 7832 URB PRO</strong></p>
-		<p style="margin-top: -20px;"><strong>Teléfono: 01 5390547</strong></p>
-		<p style="margin-top: -20px;"><strong>WhatsApp: 940 309 506</strong></p>
+ 
+    <p><strong>CONSULTORIO FEMENINO INTIMAS</strong></p>
+		<p style="margin-top: -20px;"><strong>RUC: 10225155092</strong></p>
+    <p style="margin-top: -10px;"><strong>Dirección: AV.Metroplitana 109 Ate (2DO PISO)</strong></p>
+		<p style="margin-top: -20px;"><strong>Teléfono: 968 481 100</strong></p>
 	
-    @endif
-		
 
 
 
