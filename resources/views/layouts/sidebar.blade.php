d062d4c5904e8e757e813ac32927c252bda22d22
Erik Zerpa <ezerpa@sipecom.com>
2023-02-16 13:03:03 -0400

    aaa

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1453,7 +1453,29 @@
           </li>
 
 
-        
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tools"></i>
+              <p>
+                Anotaciones
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+            <li class="nav-item">
+                <a href="{{route('anotaciones.index')}}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Lista</p>
+                </a>
+              </li>
+              
+            
+            
+              
+            </ul>
+          </li>
+
 
        
          
@@ -1691,7 +1713,29 @@
         
 
        
-         
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tools"></i>
+              <p>
+                Anotaciones
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+
+            <li class="nav-item">
+                <a href="{{route('anotaciones.index')}}" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Lista</p>
+                </a>
+              </li>
+              
+            
+            
+              
+            </ul>
+          </li>
+
 
         
 
